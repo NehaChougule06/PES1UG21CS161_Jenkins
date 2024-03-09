fa60b6e32b330f8d02e81842644126ffd28d70d8
Neha Chougule <116542315+NehaChougule06@users.noreply.github.com>
2024-03-09 23:10:42 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,6 +7,6 @@ int main(void) {
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   cout << "Using github webhook\n";
-  cout << "Made changes\n";
+  cout << "Made change\n";
   return 0;
 }
